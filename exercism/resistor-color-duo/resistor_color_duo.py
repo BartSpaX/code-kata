680d7b4e13af567eab3d702da51c5d583e5ed828
BartSpax <bspaleniak@gmail.com>
2024-03-12 11:48:56 +0100

    add resistor-color-duo exercise

---
+++ exercism/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,3 @@
+def value(colors: list):
+    list_of_colors =  ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+    return int(str(list_of_colors.index(colors[0])) + str(list_of_colors.index(colors[1])))
