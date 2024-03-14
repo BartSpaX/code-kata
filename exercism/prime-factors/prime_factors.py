11302eb3aef6330ea82f54dfbde5f1346326c73f
BartSpax <bspaleniak@gmail.com>
2024-03-14 12:41:35 +0100

    add prime-factors exercise

---
+++ exercism/prime-factors/prime_factors.py
@@ -0,0 +1,16 @@
+import math
+
+def factors(value):
+    prime_factors = []
+    
+    while value % 2 == 0:
+        prime_factors.append(2)
+        value //= 2
+    for i in range(3, int(math.sqrt(value)) + 1, 2):
+        while value % i == 0:
+            prime_factors.append(i)
+            value //= i
+
+    if value > 2:
+        prime_factors.append(value)
+    return prime_factors
